d9257622028e114a3ccd0d2aa4d60136acfa4736
SahruzRiyad <sahruzzaman01@gmail.com>
2025-02-26 12:27:24 +0600

    nullable never and unknown types added

---
+++ TypeScript/module1/src/9_nullable_never_unknown_type.ts
@@ -0,0 +1,48 @@
+{
+
+// Nullable types
+
+const searchName = (value: string | null) => {
+    if(value){
+        console.log("Searching")
+    }else{
+        console.log("There is nothing to search")
+    }
+};
+
+searchName(null);
+
+
+// Unknown types -> typeof
+
+const getSpeed = (value: unknown) => {
+    if(typeof value === "number"){
+        const convertedSpeed = (value * 1000) / 3600;
+        console.log(`The speed is of ${value}  = ${convertedSpeed} ms^-1`);
+    }
+
+    else if(typeof value === "string"){
+        const [speed, unit] = value.split(" ");
+        const convertedSpeed = (parseFloat(speed) * 1000) / 3600;
+        console.log(`The speed is of ${value} = ${convertedSpeed} ms^-1`);
+    }
+
+    else{
+        console.log("Input Fromat is not allowed");
+    }
+}
+
+getSpeed(1000);
+getSpeed(`1000 kmph`)
+getSpeed(null)
+
+
+// Never types -> never return anything
+
+const throwError = (msg: string): never => {
+    throw new Error(msg);
+}
+
+throwError("Error occurs in the process.")
+
+}
